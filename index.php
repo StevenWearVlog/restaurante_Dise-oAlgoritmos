2a74b9e6b269749ac8a04ab1867f62611244287a
StevenWearVlog <johanstevensarmientocruz800@gmail.com>
2025-09-19 16:03:13 -0500

    si llega a ser el index en base a la guia

--- index.php
+++ index.php
@@ -1,7 +1,27 @@
+<?php
+include("admin/bd.php");
+$sentencia = $conn->prepare("SELECT * FROM banner LIMIT 1");
+$sentencia->execute();
+$listaBanner = $sentencia->fetchAll(PDO::FETCH_ASSOC);
+
+$chef1 = $conn->prepare("SELECT * FROM colaboradores");
+$chef1->execute();
+$chefLista1 = $chef1->fetchAll(PDO::FETCH_ASSOC);
+
+
+$testimonios = $conn->prepare("SELECT * FROM testimonios");
+$testimonios->execute();
+$testimoniosList = $testimonios->fetchAll(PDO::FETCH_ASSOC);
+
+$platos = $conn->prepare("SELECT * FROM menu");
+$platos->execute();
+$menuLista = $platos->fetchAll(PDO::FETCH_ASSOC);
+?>
+
 <!doctype html>
 <html lang="en">
     <head>
-        <title>Title</title>
+        <title>Restaurante</title>
         <!-- Required meta tags -->
         <meta charset="utf-8" />
         <meta
@@ -23,24 +43,38 @@
 
 <nav class="navbar navbar-expand navbar-dark bg-dark">
     <div class="container">
-            <a class="navbar-brand" href=""#banner">Restaurante</a>
-    <ul class="nav navbar-nav">
-        <li class="nav-item">
-            <a class="nav-link" href="#Menu" aria-current="page"
-                >Menu del dia <span class="visually-hidden">(current)</span></a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#cheff">Chefs</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#testimonios" aria-current="page"
-                >Testimonios <span class="visually-hidden">(current)</span></a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#contacto">Contacto</a>
-        </li>
-    </ul>
-    </div>
+<a class="navbar-brand" href="#">Restaurante </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-item nav-link active" href="#banner" aria-current="page">Inicio<span class="visually-hidden">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#Chef">Chef</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#menu">Menu</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#testimonios">Testimonios</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#contacto">Contacto</a>
+                    </li>
+                </ul>
+
+            </div>
+
+        </div>
 
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
     <div class="collapse #navbar-collapse" id="navbarNav"></div>
@@ -52,6 +86,14 @@
     <div class="banner-img" style="position: relative; background: url('/images/banner.jpg') center/cover no-repeat; height: 400px;">
 
         <div class="banner-text" style="position:absolute; top:50%; left:50%; transform: translate(-50%, -50%); text-align:center; color:#fff;">
+
+        <?php foreach ($listaBanner as $banner): ?>
+
+                    <h1><?php echo $banner["titulo"]; ?></h1>
+                    <p><?php echo $banner["descripcion"] ?></p>
+                    <a href="<?php echo $banner["link"] ?>" class="btn btn-primary">Ver menú</a>
+                <?php endforeach ?>
+            
             <h1>Restaurante Freddy</h1>
             <p>Bienvenido a nuestro restaurante</p>
             <a href="menu" class="btn btn-primary">Reserva tu mesa</a>
@@ -59,161 +101,125 @@
     </div>
 </section>
 <br>
-<section id="cheff" style="margin-top:10px;" >
+<section id="Chef" style="margin-top:10px;" >
 
-    <div class="d-flex justify-content-center gap-3">
-        <div class="card h-100" style="width: 18rem;">
-            <img src="/images/81896100-cooking-profession-and-people-concept-happy-male-chef-cook-with-crossed-hands-at-restaurant.jpg" class="card-img-top" alt="Bandeja Paisa">
-            <div class="card-body">
-                <h5 class="card-title">Patricio</h5>
-                <p class="card-text small"><strong>Ingredientes</strong></p>
-                <p class="card-text">Precio: 220000</p>
-            </div>
+        <br>
+        <div class="row row-cols-3 row-cols-md-d g-4">
+            
+            <h2>Nuestros Chefs</h2>
+            <?php foreach ($chefLista1 as $chef): ?>
+                <div class="col d-flex justify-content-center">
+                    <div class="card h-100">
+
+
+                        <img src="images/<?php echo $chef["foto"]; ?>" alt="Chef1" style="max-width:500px; border-radius:9px">
+                        <div class="card-body">
+
+                            <h5 class="card-title"><?php echo $chef["nombre"]; ?></h5>
+                            <p class="card-text small"><strong><?php echo $chef["descripcion"]; ?></strong></p>
+                            <div>
+                                <?php if (!empty($chef['linkinstagram'])): ?>
+                                    <a href="<?php echo htmlspecialchars($chef['linkinstagram']); ?>"
+                                        target="_blank" rel="noopener">
+                                        <img src="images/instagram.svg" alt="insta" style="width:24px">
+                                    </a>
+                                <?php endif; ?>
+
+                                <?php if (!empty($chef['linkyoutube'])): ?>
+                                    <a href="<?php echo htmlspecialchars($chef['linkyoutube']); ?>"
+                                        target="_blank" rel="noopener">
+                                        <img src="images/youtube.svg" alt="youtube" style="width:24px">
+                                    </a>
+                                <?php endif; ?>
+
+                                <?php if (!empty($chef['linkfacebook'])): ?>
+                                    <a href="<?php echo htmlspecialchars($chef['linkfacebook']); ?>"
+                                        target="_blank" rel="noopener">
+                                        <img src="images/facebook.svg" alt="face" style="width:24px">
+                                    </a>
+                                <?php endif; ?>
+                            </div>
+
+                        </div>
+
+
+                    </div>
+
+                </div>
+            <?php endforeach ?>
         </div>
 
-        <div class="card h-100" style="width: 18rem;">
-            <img src="/images/45904861-cooking-profession-vegetarian-food-and-people-concept-happy-male-chef-chopping-pepper-over.jpg" class="card-img-top" alt="Bandeja Paisa">
-            <div class="card-body">
-                <h5 class="card-title">Patroclo</h5>
-                <p class="card-text small"><strong>Ingredientes</strong></p>
-                <p class="card-text">Precio: 220000</p>
-            </div>
+
+        </div>
         </div>
 
-        <div class="card h-100" style="width: 18rem;">
-            <img src="/images/chef-qsp56rmf4us7gld77kmxdk7ouzb0hh3kd0sp5pjwhc.jpg" class="card-img-top" alt="Bandeja Paisa">
-            <div class="card-body">
-                <h5 class="card-title">Jorge</h5>
-                <p class="card-text small"><strong>Ingredientes</strong></p>
-                <p class="card-text">Precio: 220000</p>
-            </div>
-    </div>
-</div>
 </section>
 
 <section id="testimonios" class="bg-light py-5">
     <div class="container">
         <h2 class="text-center mb-4"> Testimonios</h2>
         <div class="row">
+            
 
-            <div class="col-md 6 d-flex">
-                <div class="card mb-4 w-100">
-                    <div class="card-body">
-                        <p class="card-text"> Sirven muy buena comida</p>
-                    </div>
-                    <div class="card-footer text-muted">
-                        Oscar Jimenez
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-6 d-flex">
-                <div class="card mb-4 w-100">
-                    <div class="card-body">
-                        <p class="card-text">Muy buena atencion</p>
-                    </div>
-                    <div class="card-footer text-muted">
-                        Pedro Mondragon
-                    </div>
-                </div>
-            </div>
+           <?php foreach ($testimoniosList as $testimonio): ?>
 
-            <section>
-                <h2 class="text-center">Recomendados</h2>
-
-                <br>
-        
-                <div class="row row-cols-1 row-cols-md-d g-4">
-
-                    <div class="d-flex justify-content-center gap-3">
-    <div class="card h-100" style="width: 18rem;" id="menu">
-        <img src="/images/Bandeja_paisa,_plato_Colombiano.jpg" class="card-img-top" alt="Bandeja Paisa">
-        <div class="card-body">
-            <h5 class="card-title">Bandeja Paisa</h5>
-            <p class="card-text small"><strong>Ingredientes</strong></p>
-            <p class="card-text">Precio: 220000</p>
-        </div>
-    </div>
-
-    <div class="card h-100" style="width: 18rem;">
-        <img src="/images/Bandeja_paisa,_plato_Colombiano.jpg" class="card-img-top" alt="Bandeja Paisa">
-        <div class="card-body">
-            <h5 class="card-title">Bandeja Paisa</h5>
-            <p class="card-text small"><strong>Ingredientes</strong></p>
-            <p class="card-text">Precio: 220000</p>
-        </div>
-    </div>
-
-    <div class="card h-100" style="width: 18rem;">
-        <img src="/images/Bandeja_paisa,_plato_Colombiano.jpg" class="card-img-top" alt="Bandeja Paisa">
-        <div class="card-body">
-            <h5 class="card-title">Bandeja Paisa</h5>
-            <p class="card-text small"><strong>Ingredientes</strong></p>
-            <p class="card-text">Precio: 220000</p>
-        </div>
-    </div>
-    <div class="card h-100" style="width: 18rem;">
-        <img src="/images/Bandeja_paisa,_plato_Colombiano.jpg" class="card-img-top" alt="Bandeja Paisa">
-        <div class="card-body">
-            <h5 class="card-title">Bandeja Paisa</h5>
-            <p class="card-text small"><strong>Ingredientes</strong></p>
-            <p class="card-text">Precio: 220000</p>
-        </div>
-    </div>
-</div>
-
-                            
-                        </div>
+                    <div class="col-md-6 d-flex">
+                        <div class="card mb-4 w-100">
+                            <div class="card-body">
+                                <p class="card-text"><?php echo $testimonio["opinion"] ?></p>
+                            </div>
+                            <div class="card-footer text-muted">
+                                <?php echo $testimonio["nombre"] ?>
+                            </div>
                         </div>
                     </div>
-                    
-                    
-                </div>
-            </section>
+                <?php endforeach ?>
 
-
-        </div>
-    </div>
-
-</section>
-
-<section id="contacto" class="container mt-4"> <br>
-    <h2>Contacto</h2>
-    <p>Para cualquier consulta o pedido, no dudes en contactarte con nosotros</p>
-    <form action="?" method="post">
-
-    <div class="form-group">
-        <label for="nombre">Name</label>
-        <input
-            type="text"
-            name="nombre"
-            id="nombre"
-            class="form-control"
-            placeholder="Ingrese su nombre"/> <br>
-            <input
-            type="email"
-            name="email"
-            id="email"
-            class="form-control"
-            placeholder="Ingrese su correo electronico"/> <br>
-            <input
-            type="text"
-            name="telefono"
-            id="telefono"
-            class="form-control"
-            placeholder="Ingrese su numero de telefono"/> <br>
-            <div class="mb-3">
-                <label for="Mensaje" class="form-label">Mensaje</label>
-                <textarea class="form-control" name="mensaje" id="mensaje" rows="6" placeholder="Escriba su mensaje"></textarea>
-            </div>
+ <section id="menu">
+        <div class="container">
+            <h2 class="text-center">Recomendados</h2>
             <br>
-            <input type="submit" class="btn btn-primary" value="enviar" id="enviar">
-</br>
-</br>
-</br>
-    </div>
-    </form>
-    
-</section>
+            <div class="row row-cols-1 row-cols-md-3 g-4">
+                <?php foreach ($menuLista as $menu): ?>
+                    <div class="col d-flex justify-content-center">
+
+                        <div class="card h-100">
+                            <img src="images/<?php echo $menu["foto"] ?>" alt="Bandeja Paisa" class="card-img-top rounded-3">
+                            <div class="card-body">
+                                <h5 class="card-title"><?php echo $menu["nombre"] ?></h5>
+                                <p class="card-text small"><strong>Ingredientes: </strong><?php echo $menu["ingredientes"] ?></p>
+                                <p class="card-text"><strong>Precio: </strong><?php echo $menu["precio"] ?><strong>$</strong></p>
+                            </div>
+                        </div>
+
+
+                    </div>
+                <?php endforeach ?>
+
+            </div>
+
+        </div>
+
+    <section id="contacto" class="container mt-4"><br>
+        <h2>Contacto</h2>
+        <p>Para cualquier consulta o pedido, no dudes en contactarte con nosotros.</p>
+        <form action="admin/seccion/comentarios/crear.php" method="POST">
+
+
+            <div class="form-group">
+                <label for="nombre">Nombre: </label>
+                <input type="text" class="form-control" id="nombre" name="nombre" placeholder="Ingrese su nombre"><br>
+                <input type="email" class="form-control" id="correo" name="correo" placeholder="Ingrese su correo electrónico" required><br>
+                <div class="mb-3">
+                    <label for="Mensaje" class="form-label">Mensaje</label>
+                    <textarea class="form-control" id="mensaje" name="mensaje" rows="6" placeholder="Escriba su mensaje"></textarea>
+                </div>
+                <br>
+                <input type="submit" class="btn btn-primary" value="Enviar mensaje" name="enviar" id="">
+            </div>
+        </form>
+
+    </section>
 <br>
 
 <section id="horarios" class="container my-5">
